dac0483b234ca26d4affba2bb9916122093a2d31
Victor <ojedavd@gmail.com>
2021-09-06 23:17:25 -0300

    Consulta certificado

--- certificados/admin.py
+++ certificados/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Certificado
 
-# Register your models here.
-admin.site.register(Certificado)
+# Register your models here.  
+class CertificadoAdmin(admin.ModelAdmin):
+    list_display = ('dni', 'name', 'description', 'published')
+
+# Register the admin class with the associated model
+admin.site.register(Certificado, CertificadoAdmin)
--- certificados/views.py
+++ certificados/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Certificado
 
-# Create your views here.
+
+def index(request):
+    queryset = request.GET.get("buscar")  
+    certificados = []
+
+    if queryset:
+        certificados = Certificado.objects.filter(dni = queryset)
+
+
+    return render(request, 'certificados/index.html', {'latest_question_list': certificados})
+
